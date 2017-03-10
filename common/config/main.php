18e1b4752a739a235b2f3752f085049faf053367
mixartemev <mixartemev@gmail.com>
2017-03-10 22:00:50 +0300

    fk

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,5 @@
 <?php
-return [
+$config = [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
@@ -7,3 +7,25 @@ return [
         ],
     ],
 ];
+if (!YII_ENV_TEST) {
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][] = 'gii';
+	//php yii gii/construct --template=const
+	$config['modules']['gii'] = [
+		'class' => 'yii\gii\Module',
+		'generators' => [
+			// Имя генератора
+			'construct' => [
+				// Класс генератора
+				'class'     => 'yii\gii\generators\crud\Generator',
+				// Настройки шаблонов
+				'templates' => [
+					// Имя шаблона => путь к шаблону
+					'const' => '@common/components/generators/crud/construct',
+				]
+			],
+		],
+	];
+}
+
+return $config;
