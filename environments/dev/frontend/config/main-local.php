c79d99f03bf7410e3e2d4e7f12406ccf7a6e1bbb
mix <mixartemev@gmail.com>
2017-04-04 22:09:36 +0300

    rus labels

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -15,10 +15,11 @@ if (!YII_ENV_TEST) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
-    $config['bootstrap'][] = 'gii';
+
+    /*$config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-    ];
+    ];*/
 }
 
 return $config;
